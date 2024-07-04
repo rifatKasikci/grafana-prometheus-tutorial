cb20f227303eb4a97f5e81971a9747afabba0533
Rıfat Kaşıkcı <rifatkasikci@gmail.com>
2024-07-04 23:30:36 +0300

    Docker compose update and prometheus client added

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
-
+const client = require('prom-client');
 const app = express();
 
-app.use(express.json());
+// Default metrics
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.listen(5000, () => {
-    console.log('Server is running...');
+  console.log('Server is running on port 5000');
 });
